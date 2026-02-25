881b1af7b73bb811dc8ce1e3bff43de5db4506b3
kashiharanelson <kashiharanelson@gmail.com>
2026-02-25 02:41:17 -0300

    Add Home component with styled welcome message

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,15 @@
-
+export default function Home() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#0f0f0f",
+      color: "white",
+      fontFamily: "Arial"
+    }}>
+      <h1>PersonaAI está no ar 🚀</h1>
+    </div>
+  )
+}
